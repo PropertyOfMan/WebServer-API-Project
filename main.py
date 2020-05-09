b4b2ce65cd271599cfb366b4f653aa3f18fb0244
PropertyManager <1stintheworldpropertymanager@gmail.com>
2020-05-10 00:25:24 +0300

    Useful version

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from flask import Flask
 from data import db_session
 from data.database import Phrases
 from random import randint
+import _sqlite3
+
+
+dictionary = {1: ('В золотом окне востока лик свой солнце показало.', 'Ромео и Джульетта'), 2: ('— Через столько лет? — Всегда.', 'Северус Снегг (Северус Снейп)'), 3: ('Ни одного правителя не поддерживают все до единого.', 'Игра престолов'), 4: ('Он коснулся пальцами ее волос, она ощутила, что к ней прикоснулась любовь.', 'Сцены из жизни за стеной'), 5: ('Воины-победители сперва побеждают и только потом вступают в битву; те же, что терпят поражение, сперва вступают в битву и только затем пытаются победить.', 'Сунь-Цзы')}
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -20,12 +26,16 @@ def index():
 
 @app.route('/random')
 def random():
-    for i in session.query(Phrases).filter(Phrases.id == randint(1, len(session.query(Phrases).all()))):
-        needed = i
-    return render_template('base2.html', theme=needed.phrase, author=needed.author)
+    x = randint(1, len(dictionary))
+    print(x)
+    theme, author = dictionary.get(x)
+    return render_template('base2.html', theme=theme, author=author)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
-    db_session.global_init("db/my_db.sqlite")
-    session = db_session.create_session()
     app.run(host='localhost', port=80, debug=True)
--- tmp.py
+++ tmp.py
@@ -1,2 +1,7 @@
-import random
-print(random.randint(4, 10))
+import _sqlite3
+
+con = _sqlite3.connect('my_db.sqlite')
+cur = con.cursor()
+
+result = cur.execute("""SELECT * FROM phrases""").fetchall()
+print(len(result))
